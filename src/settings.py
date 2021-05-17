18bbaeb3526f2ddbe5546fdbee61b02d83818f74
vikassnwl <vikassnwl@gmail.com>
2021-05-17 17:53:49 +0530

    updated aliens speed in settings and fixed image paths

--- src/settings.py
+++ src/settings.py
@@ -18,7 +18,7 @@ class Settings:
         self.bullets_allowed = 3
 
         # Alien settings
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 1
 
         # How quickly the game speeds up
         self.speedup_scale = 1.1
@@ -31,7 +31,7 @@ class Settings:
         """Initialize settings that change throughout the game."""
         self.ship_speed_factor = 1.5
         self.bullet_speed_factor = 3
-        self.alien_speed_factor = 1
+        self.alien_speed_factor = .5
 
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
